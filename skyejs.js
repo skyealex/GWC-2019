2a4944c247a945ede3ab10368fe43f7353dfc297
skyealex <skye.rothstein@gmail.com>
2019-08-02 10:37:27 -0400

    Initial commit

---
+++ skyejs.js
@@ -0,0 +1,25 @@
+
+// function show_slideshow(){
+//   document.getElementById("photos1").style.display= "block";
+//   // /*wait 5 seconds*/
+//   // document.getElementById("photos1").style.display= "none";
+//   // document.getElementById("photos2").style.display= "block";
+//   // /*wait 5 seconds*/
+//   // document.getElementById("photos2").style.display= "none";
+//   // document.getElementById("photos3").style.display= "block";
+// }
+//
+// function hide_slideshow(){
+//   document.getElementById("photos1").style.display= "none";
+//   document.getElementById("photos2").style.display= "none";
+//   document.getElementById("photos3").style.display= "none";
+// }
+//
+//
+// function hidephotos(){
+//   document.slideshow.style.display="none";
+// }
+//
+// function showphotos(){
+//   document.slideshow.style.display="block";
+// }
